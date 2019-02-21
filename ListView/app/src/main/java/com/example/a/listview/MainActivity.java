28782d0af974a420a4cd41b309c29b6c80b889e3
Your Name <you@example.com>
2019-02-21 13:22:32 +0900

    listView

---
+++ ListView/app/src/main/java/com/example/a/listview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] myData = {"hello","world", "oracle", "java",
+            "hello","world", "oracle", "java",
+            "hello","world", "oracle", "java",
+            "hello","world", "oracle", "java"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, myData
+        );
+        listView.setAdapter(adapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, myData[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
