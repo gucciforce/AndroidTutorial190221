aa8e8e424a4b9145080d5bc1af28cf3e5352dc27
Your Name <you@example.com>
2019-02-21 16:48:04 +0900

    bind service

---
+++ Service2/app/src/main/java/com/example/a/service2/MyService.java
@@ -0,0 +1,31 @@
+package com.example.a.service2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.util.Random;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    public class MyBinder extends Binder{
+        public MyService getService(){
+            return MyService.this;
+        }
+    }
+
+    MyBinder myBinder = new MyBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return myBinder;
+    }
+
+    Random random = new Random();
+    public int getRandomNumber(){
+        return random.nextInt(100);
+    }
+}
