5e453a5ba9fb2f997240dc39b856bd803abdc62f
Your Name <you@example.com>
2019-02-21 14:35:19 +0900

    Thread

---
+++ Thread/app/src/main/java/com/example/a/thread/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.a.thread;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStart;
+    class MyThread extends Thread{
+        @Override
+        public void run(){
+            for(int i=0;i<100;i++) {
+                Log.d("count", "count : " + i);
+                Message msg = handler.obtainMessage();
+                msg.what = 1;
+                msg.arg1 = i;
+                handler.sendMessage(msg);
+
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //쓰레드 + 메세지 + 핸들러
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 1){
+                btnStart.setText("count:"+msg.arg1);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                MyThread th = new MyThread();
+                th.start();
+            }
+        });
+    }
+}
