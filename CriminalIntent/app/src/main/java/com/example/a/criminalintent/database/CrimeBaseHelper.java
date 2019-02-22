c750609dfe813f6700267d4d404d78e3915f96f8
Your Name <you@example.com>
2019-02-22 16:43:50 +0900

    crime

--- CriminalIntent/app/src/main/java/com/example/a/criminalintent/database/CrimeBaseHelper.java
+++ CriminalIntent/app/src/main/java/com/example/a/criminalintent/database/CrimeBaseHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.a.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 public class CrimeBaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "crimeBase.db";
     private static final int VERSION = 1;
@@ -13,6 +15,12 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+CrimeTable.NAME+"(_id integer primary key autoincrement, "+
+                CrimeTable.Cols.UUID + ", "+
+                CrimeTable.Cols.TITLE + ", "+
+                CrimeTable.Cols.DATE + ", "+
+                CrimeTable.Cols.SOLVED + ")"
+        );
 
     }
 
