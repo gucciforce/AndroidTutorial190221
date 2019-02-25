a724a141e6d969624a985d8f68557ce33c9b4834
Your Name <you@example.com>
2019-02-25 14:58:06 +0900

    beatbox

--- BeatBox/app/src/main/java/com/example/a/beatbox/model/BeatBox.java
+++ BeatBox/app/src/main/java/com/example/a/beatbox/model/BeatBox.java
@@ -2,6 +2,9 @@ package com.example.a.beatbox.model;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class BeatBox {
     private static final String TAG = "BeatBox";
@@ -10,5 +13,16 @@ public class BeatBox {
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds(){
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUND_FOLDER);
+            Log.d(TAG, "Found "+soundNames.length+" sounds.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
