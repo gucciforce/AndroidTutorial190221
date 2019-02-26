34b8ace94a4024f12178940fabad7352d3810bd4
Your Name <you@example.com>
2019-02-26 10:59:51 +0900

    xmlpullparser

---
+++ XmlPullParser/app/src/main/java/com/example/a/xmlpullparser/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.a.xmlpullparser;
+
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+//http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153051000
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    class WeatherData{
+        int day;
+        int hour;
+        float temp;
+        String wfKor;
+
+        @Override
+        public String toString() {
+            return "day : "+day + " hour : "+ hour + " temp : "+ temp + " wfKor : " + wfKor;
+        }
+    }
+    ArrayList<WeatherData> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btnWeather).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        MyPullParserTask text = new MyPullParserTask();
+        text.execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153051000");
+    }
+
+    class MyPullParserTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
+}
